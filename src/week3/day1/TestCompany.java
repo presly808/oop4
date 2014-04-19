278124fb5b592c54add79e555182c408a93840d1
presly808 <presly808@gmail.com>
2014-04-19 11:31:03 +0300

    day1 part commit

---
+++ src/week3/day1/TestCompany.java
@@ -0,0 +1,11 @@
+package week3.day1;
+
+public class TestCompany {
+
+	public static void main(String[] args) {
+		Tester t1 = new Tester("Manual");
+		t1.printInfo();
+
+	}
+
+}
---
+++ src/week3/day1/Tester.java
@@ -0,0 +1,33 @@
+package week3.day1;
+
+public class Tester extends Employee {
+
+	private String type;
+	
+	public Tester(){
+		
+	}
+	
+	public Tester(String type){
+		super();
+		this.type = type;
+	}
+	
+	public Tester(String name, int age, double salary, String type){
+		super(name, age, salary);
+		this.type = type;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public void go(){
+		
+	}
+	
+}
